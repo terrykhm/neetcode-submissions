9d016ffef303ed79d70f8abec80b52c3d7e2f31a
Terry Kim <terry97@terpmail.umd.edu>
2026-07-01 00:12:28 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        sumDict = defaultdict(int)
+        i = 1
+        sumDict[numbers[0]] = i
+
+        i += 1
+        while i <= len(numbers):
+            targetDiff = target - numbers[i - 1]
+
+            if targetDiff in sumDict:
+                return [sumDict[targetDiff], i]
+            else:
+                sumDict[numbers[i - 1]] = i
+                i +=1
+        
+        return []
